c041a07a2e5c616165913352526d34c1a0c7486b
Andrea-gm <162758982+Andrea-gm@users.noreply.github.com>
2025-09-19 18:00:09 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,205 @@
+# AutoXSp  
+**Automated Compositional Characterization via X-Ray Spectroscopy at Electron Microscopes**
+
+AutoXSp is a **fully automated framework** for SEM-EDS workflows — from spectral acquisition and quantification to data filtering and compositional analysis — all in **one click**.
+
+### ✨ Key Features
+- **Automated acquisition & quantification** of X-ray spectra. Single spectrum quantification script available as well
+- **Rule-based filtering** to automatically discard poorly quantified spectra from the analysis
+- **Machine learning–based compositional analysis** to identify the compositions of individual phases in the sample  
+- **Automated experimental standard collection** scripts included
+- **Extensible architecture** — adaptable to other techniques such as  
+  - Wavelength Dispersive Spectroscopy (WDS)  
+  - Scanning Transmission Electron Microscopy (STEM) with EDS  
+- **Extensible hardware support** — includes driver for ThermoFisher Phenom Desktop SEM series, and can be extended to any electron microscope with a Python API  
+
+### 📊 Performance
+- **Benchmarked** on 74 single-phase samples with compositions spanning **38 elements** (from nitrogen to bismuth), it achieved **<5–10% relative deviation** from expected values  
+    *(See publication: TO ADD)* 
+- **Machine learning** compositional analysis detects individual phase composition in **multi-phase samples**, including minor phases
+- **Intermixed phases** can also be resolved
+
+### 🧪 Supported Use Cases
+- Powder, bulk, and rough samples  
+- Scanning Electron Microscopy (SEM) with Energy-Dispersive Spectroscopy (EDS)  
+
+### ⚙️ Requirements
+- Works on all major platforms  
+- Quick installation  
+- Requires some calibration for use with different electron microscopes  
+
+---
+
+## 🚀 Installation
+
+You can install **AutoXSp** in just one command.
+
+### Using pip (Python Package Index)
+pip install autoxsp
+
+### Or directly from GitHub:
+pip install git+https://github.com/CederGroupHub/AutoXSp
+
 # AutoXSp
-Automated Compositional Characterization of Powders via X-Ray Spectroscopy at an Electron Microscope
+
+[![PyPI version](https://badge.fury.io/py/autoxsp.svg)](https://pypi.org/project/autoxsp/)
+[![Python Version](https://img.shields.io/pypi/pyversions/autoxsp.svg)](https://pypi.org/project/autoxsp/)
+[![License](https://img.shields.io/github/license/<your-github-username>/AutoXSp.svg)](LICENSE)
+[![Downloads](https://pepy.tech/badge/autoxsp)](https://pepy.tech/project/autoxsp)
+
+**AutoXSp** — Automated Compositional Characterization via X-Ray Spectroscopy at an Electron Microscope.  
+A fast, flexible, and easy-to-use Python package for processing and analyzing X-ray spectral data.  
+Designed for scientists, engineers, and researchers who want quick, reproducible results.
+
+---
+
+## 📑 Table of Contents
+- [✨ Features](#-features)
+- [🎥 Demo](#-demo)
+- [🆕 Coming Soon](#-coming-soon)
+- [🚀 Installation](#-installation)
+- [🖥 Use](#-use)
+- [📦 Requirements](#-requirements)
+- [📂 Project Structure](#-project-structure)
+- [🤝 Contributing](#-contributing)
+- [📄 License](#-license)
+- [📬 Contact](#-contact)
+
+---
+
+## ✨ Features
+- Automatic processing of X-ray spectral data
+- Open-source and customizable for non-commercial-use
+
+---
+
+## 🎥 Demo
+- Watch Auto-XSp in action on a desktop SEM-EDS system at https://youtu.be/Bym58gNxlj0
+
+---
+
+## 🆕 Coming Soon
+Here’s what’s planned for future releases of **AutoXSp**:
+- ⚡ GPU acceleration for faster data processing
+- 🐍 Upgrade to **Python 3.12** for improved performance, modern syntax features, and better compatibility with the latest scientific libraries
+- 🚀 Integration of a **forked `lmfit`** version accepting `Model.fit(data, fit_kws={'full_output': False})` to avoid covariance computations and speed up computations
+
+---
+
+## 🚀 Installation
+
+You can install **AutoXSp** in just one command.
+
+### Using pip
+pip install autoxsp
+
+### Or directly from GitHub:
+pip install git+https://github.com/CederGroupHub/AutoXSp
+
+### Using conda
+conda install -c conda-forge autoxsp
+
+---
+
+## 🖥 Quick Start
+
+AutoXSp supports two main automated workflows:
+
+1. **Experimental Standard Collection** — acquire and fit EDS/WDS spectra from known-composition samples to generate reference peak-to-background ratios.
+2. **Sample Acquisition & Analysis** — acquire spectra from unknown samples, quantify them, and perform compositional phase analysis.
+
+---
+
+### 1️⃣ Acquire Experimental Standards
+from autoxsp.runners import batch_acquire_experimental_stds
+
+# Define standards(s) to analyse (additional options available):
+# - 'ID': unique standard identifier
+# - 'formula': standard composition
+# - 'pos': stage position (x, y) in mm
+# - 'sample_type': bulk or powder
+# - 'is_manual_meas': Manually select spots if standard is not bulk, nor powder
+
+std_list = [
+    {
+        'id': 'Al_std',
+        'formula': 'Al',
+        'pos': (0, 0),
+        'sample_type': 'bulk',
+        'is_manual_meas': False
+    },
+]
+
+# Run experimental standard acquisition at the microscope computer
+batch_acquire_experimental_stds(stds=std_list)
+
+
+### 2️⃣ Acquire & Analyse Samples
+from autoxsp.runners import batch_acquire_and_analyze
+
+# Define sample(s) to analyse (additional options available):
+# - 'ID': unique sample identifier
+# - 'els': list of possible elements in the sample
+# - 'pos': stage position (x, y) in mm
+# - 'cnd' (optional): list of candidate phases/formulas
+
+samples = [
+    {
+        'id': 'Anorthite_mineral',
+        'els': ['Ca', 'Al', 'Si', 'O'],
+        'pos': (-37.5, -37.5),
+        'cnd': ['CaAl2Si2O8']
+    },
+]
+
+# Run acquisition and analysis at the microscope computer
+batch_acquire_and_analyze(samples)
+
+
+---
+
+## 📦 Requirements
+
+Python 3.8 or newer
+Dependencies are installed automatically with pip or conda
+
+Electron Microscope driver developed for the Thermofisher Phenom Desktop SEMs. Will work with any microscope of the series, equipped with PPI (Phenom Programming Interface)
+
+---
+
+## 📂 Project Structure
+
+python
+Copy code
+AutoXSp/
+├── autoxsp/           # Main package source code
+├── scripts/           # Helper scripts
+├── tests/             # Unit tests
+├── LICENSE
+├── README.md
+└── pyproject.toml
+
+---
+
+## 🤝 Contributing
+
+Contributions are welcome!
+
+Open to collaborations to extend this code to different microscopes or to different types of samples, for example thin films
+
+---
+
+## 📄 License
+
+This project is licensed under an academic, nonprofit, internal, research & development, NON-COMMERCIAL USE ONLY,
+LICENSE — see the LICENSE file for details.
+
+---
+
+## 📬 Contact
+
+For questions or issues, please open an issue on GitHub.
+
+
+---
+
---
+++ autoxsp/runners/fit_and_quantify_spectrum.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Fitting and quantification of a single X-ray spectrum.
+
+For spectrum-level analysis of fitting and quantification performance.
+
+Import this module in your own code and call the
+`fit_and_quantify_spectrum()` function, passing your desired 'sample_ID', 'spectrum_ID' and
+options as arguments. This enables integration into larger workflows or pipelines.
+
+Workflow:
+    - Loads sample configurations from `Spectra_collection_info.json`
+    - Loads acquired spectral data from `Data.csv`
+    - Performs quantification (optionally only on unquantified spectra)
+    - Optionally performs clustering/statistical analysis and saves results
+
+Notes
+-----
+- Only the `sample_ID` and 'spectrum_ID' are required if acquisition output is saved in the default Results directory;
+  otherwise, specify `results_path`.
+
+Parameters
+----------
+sample_ID : str
+    Sample identifier.
+spectrum_ID : int
+    Value reported in 'Spectrum #' column in Data.csv.
+results_path : str, optional
+    Base directory where results are stored. Default: AutoXSp/Results
+use_instrument_background : bool, optional
+    Whether to use instrument background if present.
+quantify_plot : bool, optional
+    Whether to plot quantification results.
+plot_signal : bool, optional
+    Whether to plot the signal.
+zoom_plot : bool, optional
+    Whether to zoom on a specific line.
+line_to_plot : str, optional
+    Line to zoom in plot.
+els_substrate : list, optional
+    List of substrate elements.
+fit_tol : float, optional
+    Fit tolerance.
+is_particle : bool, optional
+    If True, treat sample as particle (powder).
+max_undetectable_w_fr : float, optional
+    Maximum allowed weight fraction for undetectable elements (default: 0).
+force_single_iteration : bool, optional
+    If True, quantification will be run for a single iteration only (default: False).
+interrupt_fits_bad_spectra : bool, optional
+    If True, interrupt fitting if bad spectra are detected (default: False).
+    
+Created on Tue Jul 29 13:18:16 2025
+
+@author: Andrea
+"""
+
+import os
+import warnings
+import time
+import logging
+
+from autoxsp.tools.utils import (
+    print_double_separator,
+    get_sample_dir,
+    load_configurations_from_json,
+    extract_spectral_data
+)
+import autoxsp.tools.constants as cnst
+from autoxsp.tools.config_classes import config_classes_dict, ExpStandardsConfig
+from autoxsp.core.XSp_quantifier import XSp_Quantifier
+from autoxsp.core.EMXSp_composition_analyser import EMXSp_Composition_Analyzer
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+def fit_and_quantify_spectrum(
+    sample_ID: str,
+    spectrum_ID: int,
+    spectrum_lims: tuple = None,
+    results_path: str = None,
+    use_instrument_background: bool = False,
+    quantify_plot: bool = True,
+    plot_signal: bool = True,
+    zoom_plot: bool = False,
+    line_to_plot: str = '',
+    els_substrate: list = None,
+    fit_tol: float = 1e-4,
+    is_particle: bool = True,
+    max_undetectable_w_fr: float = 0,
+    force_single_iteration: bool = False,
+    interrupt_fits_bad_spectra: bool = False,
+):
+    """
+    Fit and (optionally) quantify a single spectrum.
+
+    Parameters
+    ----------
+    sample_ID : str
+        Sample identifier.
+    spectrum_ID : int
+        Value reported in 'Spectrum #' column in Data.csv.
+    results_path : str, optional
+        Base directory where results are stored. Default: AutoXSp/Results
+    use_instrument_background : bool, optional
+        Whether to use instrument background if present.
+    quantify_plot : bool, optional
+        Whether to plot quantification results.
+    plot_signal : bool, optional
+        Whether to plot the signal.
+    zoom_plot : bool, optional
+        Whether to zoom on a specific line.
+    line_to_plot : str, optional
+        Line to zoom in plot.
+    els_substrate : list, optional
+        List of substrate elements.
+    fit_tol : float, optional
+        Fit tolerance.
+    is_particle : bool, optional
+        If True, treat sample as particle (powder).
+    max_undetectable_w_fr : float, optional
+        Maximum allowed weight fraction for undetectable elements (default: 0).
+    force_single_iteration : bool, optional
+        If True, quantification will be run for a single iteration only (default: False).
+    interrupt_fits_bad_spectra : bool, optional
+        If True, interrupt fitting if bad spectra are detected (default: False).
+        
+    Returns
+    -------
+    quantifier : XSp_Quantifier
+        The quantifier object containing the results, fit parameters, and methods for further analysis and plotting.
+    """
+    if results_path is None:
+        parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        results_path = os.path.join(parent_dir, cnst.RESULTS_DIR)
+        
+    sample_dir = get_sample_dir(results_path, sample_ID)
+    spectral_info_f_path = os.path.join(sample_dir, f"{cnst.ACQUISITION_INFO_FILENAME}.json")
+    data_path = os.path.join(sample_dir, f"{cnst.DATA_FILENAME}.csv")
+    
+    print_double_separator()
+    logging.info(f"Sample '{sample_ID}'")
+    
+    try:
+        configs, metadata = load_configurations_from_json(spectral_info_f_path, config_classes_dict)
+    except FileNotFoundError:
+        logging.warning(f"Could not find {spectral_info_f_path}. Skipping sample '{sample_ID}'.")
+        return
+    except Exception as e:
+        logging.warning(f"Error loading {spectral_info_f_path}. Skipping sample '{sample_ID}': {e}")
+        return
+
+    sample_processing_time_start = time.time()
+    
+    # Retrieve configuration objects
+    try:
+        microscope_cfg      = configs[cnst.MICROSCOPE_CFG_KEY]
+        sample_cfg          = configs[cnst.SAMPLE_CFG_KEY]
+        measurement_cfg     = configs[cnst.MEASUREMENT_CFG_KEY]
+        sample_substrate_cfg= configs[cnst.SAMPLESUBSTRATE_CFG_KEY]
+        quant_cfg           = configs[cnst.QUANTIFICATION_CFG_KEY]
+        clustering_cfg      = configs[cnst.CLUSTERING_CFG_KEY]
+        powder_meas_cfg     = configs.get(cnst.POWDER_MEASUREMENT_CFG_KEY, None)  # Optional
+        exp_stds_cfg     = configs.get(cnst.EXP_STD_MEASUREMENT_CFG_KEY, None)  # Optional
+    except KeyError as e:
+        logging.warning(f"Missing configuration '{e.args[0]}' in {spectral_info_f_path}. Skipping sample '{sample_ID}'.")
+        return
+    
+    # Load experimental standard dictionary if the sample is a known powder precursor mix
+    if powder_meas_cfg and powder_meas_cfg.is_known_powder_mixture_meas:
+        if not exp_stds_cfg:
+            exp_stds_cfg = ExpStandardsConfig()
+        comp_analyzer = EMXSp_Composition_Analyzer(microscope_cfg,
+                                                   sample_cfg,
+                                                   measurement_cfg,
+                                                   sample_substrate_cfg,
+                                                   quant_cfg,
+                                                   clustering_cfg,
+                                                   powder_meas_cfg = powder_meas_cfg,
+                                                   exp_stds_cfg = exp_stds_cfg)
+        stds_dict = comp_analyzer.XSp_std_dict
+    else:
+        stds_dict = None
+    
+    # Load 'Data.csv' into a DataFrame
+    try:
+        _, spectral_data, _, original_df = extract_spectral_data(data_path)
+    except Exception as e:
+        logging.warning(f"Could not load spectral data for '{sample_ID}': {e}")
+        return
+
+    # Extract the row corresponding to spectrum_ID
+    if cnst.SP_ID_DF_KEY not in original_df.columns:
+        logging.error(f"Column '{cnst.SP_ID_DF_KEY}' not in Data.csv for sample '{sample_ID}'.")
+        return
+
+    df_match = original_df[original_df[cnst.SP_ID_DF_KEY] == spectrum_ID]
+    if df_match.empty:
+        logging.warning(f"Spectrum ID {spectrum_ID} not found in Data.csv for sample '{sample_ID}'.")
+        return
+    
+    sp_idx = df_match.index[0]
+
+    # Extract spectrum data from spectral_data (not from df_row)
+    try:
+        spectrum = spectral_data[cnst.SPECTRUM_DF_KEY][sp_idx]
+    except Exception as e:
+        logging.warning(f"Spectrum data not found for spectrum ID {spectrum_ID} in sample '{sample_ID}': {e}")
+        return
+
+    # Background extraction
+    background = None
+    bkg_list = spectral_data.get(cnst.BACKGROUND_DF_KEY)
+    
+    if use_instrument_background:
+        if (
+            bkg_list is not None
+            and isinstance(bkg_list, (list, tuple))
+            and len(bkg_list) > sp_idx
+            and bkg_list[sp_idx] is not None
+        ):
+            background = bkg_list[sp_idx]
+        else:
+            warnings.warn(
+                "Instrument background not found or empty for this spectrum. "
+                "Spectral background will be computed instead."
+            )
+
+    # Collection time
+    if 'Live_time' in spectral_data and len(spectral_data['Live_time']) > sp_idx:
+        sp_collection_time = spectral_data['Live_time'][sp_idx]
+    else:
+        sp_collection_time = None
+
+    # Calibration and configuration parameters
+    try:
+        beam_energy = measurement_cfg.beam_energy_keV
+        emergence_angle = measurement_cfg.emergence_angle
+        el_to_quantify = sample_cfg.elements
+        offset = microscope_cfg.energy_zero
+        scale = microscope_cfg.bin_width
+    except Exception as e:
+        logging.error(f"Error extracting calibration/configuration parameters: {e}")
+        return
+    
+    # Substrate elements
+    if els_substrate is None:
+        els_substrate = sample_substrate_cfg.elements
+    # Spectral limits
+    if spectrum_lims is None:
+        spectrum_lims = quant_cfg.spectrum_lims
+        
+    sp_start, sp_end = spectrum_lims
+    spectrum_vals = spectrum
+
+    # Quantification (replace with your quantifier class as needed)
+    quantifier = XSp_Quantifier(
+        spectrum_vals = spectrum_vals,
+        spectrum_lims = spectrum_lims,
+        microscope_ID = microscope_cfg.ID,
+        meas_type = measurement_cfg.type,
+        meas_mode = measurement_cfg.mode,
+        det_ch_offset=offset,
+        det_ch_width=scale,
+        beam_e=beam_energy,
+        emergence_angle=emergence_angle,
+        background_vals=background,
+        els_sample=el_to_quantify +['Si','Cl'],
+        els_substrate=els_substrate,
+        els_w_fr=None,
+        is_particle=is_particle,
+        sp_collection_time=sp_collection_time,
+        max_undetectable_w_fr=max_undetectable_w_fr,
+        fit_tol=fit_tol,
+        verbose=True,
+        fitting_verbose=True,
+        standards_dict = stds_dict
+    )
+    
+    try:
+        if quantify_plot:
+            quant_result, _, flag = quantifier.quantify_spectrum(
+                force_single_iteration=force_single_iteration,
+                interrupt_fits_bad_spectra=interrupt_fits_bad_spectra,
+                print_result=True
+                )
+        else:
+            quantifier.initialize_and_fit_spectrum(
+                print_results=True
+            )
+    except Exception as e:
+        import traceback
+        tb_str = traceback.format_exc()
+        logging.exception(f"Error during spectral quantification for '{sample_ID}': {e}\n{tb_str}")
+        return
+
+    if plot_signal:
+        line_to_zoom = line_to_plot if zoom_plot else ''
+        quantifier.plot_quantified_spectrum(
+            plot_title=f"{sample_ID}_#{spectrum_ID}",
+            peaks_to_zoom=line_to_zoom,
+            annotate_peaks='main'
+        )
+
+    total_process_time = (time.time() - sample_processing_time_start) / 60
+    print_double_separator()
+    logging.info(f"Sample '{sample_ID}' successfully quantified in {total_process_time:.1f} min.")
+        
+    return quantifier
