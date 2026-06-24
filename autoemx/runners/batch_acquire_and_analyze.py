4662df20e01edd48cc1005234db280771755aea3
Andrea-gm <162758982+Andrea-gm@users.noreply.github.com>
2026-06-24 12:05:41 -0700

    Revert "Updated xs_spot_selection"
    This reverts commit cecc40ee10f606e8235874cb0787ca9f1dbd8754.

--- autoemx/runners/batch_acquire_and_analyze.py
+++ autoemx/runners/batch_acquire_and_analyze.py
@@ -36,10 +36,7 @@ import logging
 from typing import List, Dict, Tuple, Any, Optional
 
 from autoemx.core.composition_analysis import EMXSp_Composition_Analyzer
-from autoemx.core.em_runtime.xsp_spot_selection import (
-    XSpSpotSelectionContext,
-    XSpSpotSelectorCallback,
-)
+from autoemx.core.em_runtime.xsp_spot_selection import XSpSpotSelectorCallback
 import autoemx.calibrations as calibs
 import autoemx.utils.constants as cnst
 from autoemx.utils import print_double_separator
@@ -63,7 +60,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-__all__ = ["batch_acquire_and_analyze", "XSpSpotSelectionContext", "XSpSpotSelectorCallback"]
+__all__ = ["batch_acquire_and_analyze"]
 
 def batch_acquire_and_analyze(
     samples: List[Dict[str, Any]],
